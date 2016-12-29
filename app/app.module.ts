c2f6b2b4e0139579d4d660b98aaca569022061a7
Zundux <zundux@gmail.com>
2016-12-30 05:30:24 +0700

    heroes example

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,17 +1,30 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { RouterModule }   from '@angular/router';
 
 import { AppComponent }  from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
+import { HeroService } from './hero.service';
+
+import { AppRoutingModule }     from './app-routing.module';
+
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent
   ],
+  providers: [ HeroService ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,26 +1,37 @@
-import { Component } from '@angular/core';
+// Keep the Input import for now, we'll remove it later:
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Params }   from '@angular/router';
+import { Location }                 from '@angular/common';
 
-export class Hero {
-  id: number;
-  name: string;
-}
+import 'rxjs/add/operator/switchMap';
 
-const HEROES: Hero[] = [
-  {id: 11, name: 'Mr. Nice'},
-  {id: 12, name: 'Narco'},
-  {id: 13, name: 'Bombasto'},
-  {id: 14, name: 'Celeritas'},
-  {id: 15, name: 'Magneta'},
-  {id: 16, name: 'RubberMan'},
-  {id: 17, name: 'Dynama'},
-  {id: 18, name: 'Dr IQ'},
-  {id: 19, name: 'Magma'},
-  {id: 20, name: 'Tornado'}
-];
+import { HeroService } from './hero.service';
+import { Hero } from './hero';
 
 @Component({
+  moduleId: module.id,
   selector: 'my-hero-detail',
+  templateUrl: 'hero-detail.component.html',
+  styleUrls: ['hero-detail.component.css']
 })
-export class HeroDetailComponent  {
-  selectedHero: Hero;
+export class HeroDetailComponent implements OnInit {
+
+  @Input()
+  hero: Hero;
+
+  constructor(
+    private heroService: HeroService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params
+      .switchMap((params: Params) => this.heroService.getHero(+params['id']))
+      .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
